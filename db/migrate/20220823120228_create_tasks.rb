df36f7b5806ca322983d408e092d70c48bc77fb2
ernesto baquero <ernest.baquero@gmail.com>
2022-08-23 15:49:11 +0100

    pending debug Routing Error

--- db/migrate/20220823120228_create_tasks.rb
+++ db/migrate/20220823120228_create_tasks.rb
@@ -2,10 +2,9 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :title
       t.text :details
-      t.string :completed
-      t.string :boolean, default: false
+      t.boolean :completed, default: false
 
       t.timestamps
     end

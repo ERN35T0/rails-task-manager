e3c0560811b55d4a2ae64672760f67a25a5a01e5
ernesto baquero <ernest.baquero@gmail.com>
2022-08-23 13:13:45 +0100

    add task model

---
+++ db/migrate/20220823120228_create_tasks.rb
@@ -0,0 +1,13 @@
+# frozen string literal: false
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :details
+      t.string :completed
+      t.string :boolean, default: false
+
+      t.timestamps
+    end
+  end
+end
